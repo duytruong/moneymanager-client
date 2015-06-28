76dc5ea76d76a89189743b852e8c262fee6b0706
ducduy <ducduytruong2012@gmail.com>
2015-06-29 00:37:53 +0700

    add register page, jquery-ui

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,33 +1,30 @@
-function registerUser(userData) {
-
+function register() {
+	
+	var userData = new Object();
+	userData.name = $("#txt-name").val();
+	userData.email = $("#txt-email").val();
+	userData.password = $("#txt-password").val();
+	
     $.ajax({
         url: WS_URL.REGISTER_URL,
         contentType: JSON_CONTENT_TYPE,
-        dataType: JSON_DATA_TYPE,
         type: REQUEST.POST,
         data: JSON.stringify(userData),
         success: function(data) {
-
-            //clear input fields
-            $('#reg')[0].reset();
-
-            //mark success on the registration form
-            $('#formMsgs').append($('<span class="success">Member Registered</span>'));
-
-            updateMemberTable();
+        	showDialog("Money Manager", "Your account has been created.")
         },
         error: function(error) {
-            if ((error.status == 409) || (error.status == 400)) {
-
-                var errorMsg = $.parseJSON(error.responseText);
-
-                $.each(errorMsg, function(index, val) {
-                    $('<span class="invalid">' + val + '</span>').insertAfter($('#' + index));
-                });
-            } else {
-                //console.log("error - unknown server issue");
-                $('#formMsgs').append($('<span class="invalid">Unknown server error</span>'));
-            }
+        	console.log(error);
         }
     });
+}
+
+function showDialog(dialogTitle, dialogContent) {
+	$("#dialog").dialog({
+		title: dialogTitle,
+		modal: true,
+		open: function(event, ui) {
+			$("#dialog-content").text(dialogContent);
+		}
+	});
 }
